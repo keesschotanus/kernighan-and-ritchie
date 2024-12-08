d369c19426c8c74d666766d026732d449f172d58
keesschotanus <kees.schotanus@planet.nl>
2024-12-08 11:03:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Exercises to "The C programming language"
+
+This project contains my solution of the exercises from the book
+'The C Programming Language (second edition)' by Kernighan and Ritchie.
+
+# Setup
+
+OS: Linux Mint
+C++ compiler: gcc version 11.4.0 (Ubuntu 11.4.0-1ubuntu1~22.04)
+IDE: Visual Studio Code
---
+++ ex-1-1.c
@@ -0,0 +1,20 @@
+/*
+ * Exercise 1-1
+ * Run the "hello, world" program on your system.
+ * Experiment with leaving out parts of the program,
+ * to see what error messages you get.
+ */
+
+#include <stdio.h>
+
+main()
+{
+	printf("hello, world\n");
+}
+
+// When leaving out: #include <stdio.h>
+// warning: implicit declaration of function ‘printf’
+// note: include ‘<stdio.h>’ or provide a declaration of ‘printf’
+//
+// When leaving out: main
+// undefined reference to `main'
