c235785e92a4e88c74f7dabe30a21f5be5ecce06
keesschotanus <kees.schotanus@planet.nl>
2024-12-15 13:19:11 +0100

    Added example 1-19

---
+++ ex-1-19.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#define MAXLINE 1000 /* maximum input line length */
+
+int get_line(char line[], int maxline);
+void reverse(char []);
+
+/* Reverse input line by line */ 
+int main()
+{
+	int len;				/* current line length */
+	char line[MAXLINE];		/* current input line */
+
+	while ((len = get_line(line, MAXLINE)) > 0)
+	{
+		reverse(line);
+		printf("%s", line);
+	}
+}
+
+/* get_line: read a line into s, return length */
+/* Using getline conflicted with same function from the library */
+int get_line(char s[], int lim)
+{
+	int c, i;
+	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		s[i] = c;
+	if (c == '\n')
+		s[i++] = c;
+	s[i] = '\0';
+
+	return i;
+}
+
+void reverse(char line[]) 
+{
+	// Determine length of line
+	int start, end;
+	for (end = 0; line[end] != '\0'; ++end)
+		;
+
+	--end; // Index of last char in line array
+	if (end > 0 && line[end] == '\n')
+		--end; // Skip last character if it is a newline
+
+    for (start = 0; start < end; start++, --end)
+	{
+		// Swap start char with end char
+        int temp = line[start];
+        line[start] = line[end];
+        line[end] = temp;
+    }
+}
