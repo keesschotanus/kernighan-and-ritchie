0b5fc390f7ce086d70f1914073e278368f09cf27
keesschotanus <kees.schotanus@planet.nl>
2025-01-05 18:19:07 +0100

    ex-5-6- corrected length of get_line

--- chapter-05-pointers-and-arrays/ex-5-6.c
+++ chapter-05-pointers-and-arrays/ex-5-6.c
@@ -30,8 +30,10 @@ int get_line(char *s, int lim)
 	int c, i;
 	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
 		*s++ = c;
-	if (c == '\n')
+	if (c == '\n') {
 		*s++ = c;
+		++i;
+	}
 	*s = '\0';
 
 	return i;
